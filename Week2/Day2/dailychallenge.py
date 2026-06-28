2e1b519222890e62fe38a482aed5860c9420b9de
Yaïr <berrebiyair59@gmail.com>
2026-06-28 17:27:53 +0300

    matriceexo

--- Week2/Day2/dailychallenge.py
+++ Week2/Day2/dailychallenge.py
@@ -5,40 +5,10 @@ h%x
 i ?
 sM# 
 $a 
-#t%'''  
+#t%'''
 
-matrix= [list(ligne) for ligne in MATRIX_STR.strip().split("\n")]
-print(matrix)
-
-
-nb_colonnes = len(matrix[0])
-
-for col in range(nb_colonnes):
-    for ligne in matrix:
-        if col < len(ligne):
-            print(ligne[col])  # accès par [ligne][colonne]
-
-
-resultat = ""
-
-for col in range(nb_colonnes):
-    for ligne in matrix:
-        if col < len(ligne):
-            char = ligne[col]
-            if char.isalpha():  # vérifie si c'est une lettre
-                resultat += char
-
-message_final = ""
-
-for col in range(nb_colonnes):
-    colonne_temp = ""
-    for ligne in matrix:
-        if col < len(ligne):
-            char = ligne[col]
-            if char.isalpha():
-                colonne_temp += char
-            else:
-                colonne_temp += " "  # symbole → espace
-    message_final += colonne_temp
-
-print(message_final)
+for i in range(3):
+    for j in MATRIX_STR.splitlines():
+        if len(j) > 1 and j[i].isalpha():
+            print(j[i], end="")
+print('')
---
+++ Week2/Day4/dailychallenge.py
@@ -0,0 +1,4 @@
+mot = input("enter your world separate with :, : ")
+print(mot)
+
+mot.split()
