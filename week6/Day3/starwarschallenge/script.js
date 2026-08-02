7ba421a619e1258d2a00a4548d7b0ab6556bd4a4
Yaïr <berrebiyair59@gmail.com>
2026-08-02 17:57:33 +0300

    day3exo

--- week6/Day3/starwarschallenge/script.js
+++ week6/Day3/starwarschallenge/script.js
@@ -1,106 +1,178 @@
-console.log('ver 2.3 ajax');
-// init ajax
-let xhr = new XMLHttpRequest();
+console.log("Star Wars AJAX Challenge");
 
-//Select elements in DOM
-let button = document.querySelector('#button');
-let names = document.querySelector('#name');
-let height = document.querySelector('#height');
-let gender = document.querySelector('#gender');
-let birthYear = document.querySelector('#birth-year');
-let homeWorld = document.querySelector('#home-world');
 
-//Get the info from API and catch for Errors
-function getInfo (){
-  //Call Loading Data
- 	updateWithLoading();
-  //Get Random people in the API between 1 and 88
- 	let randomNumber = Math.floor((Math.random() * 88) + 1);
-   let apiUrl = 'https://swapi.dev/api/people/' + randomNumber + '/';
-   
-  // ajax
-  xhr.open('GET', apiUrl);
-  xhr.responseType = 'json';
-  xhr.send();
-  xhr.onload = function(){
-    if(xhr.status != 200){
-      //console.log(`Error: ${xhr.status}: ${xht.statusText}`);
-      updateInfoWithError();
-      console.log('there was an error');
-    }
-    else{
-      updateInfo(xhr.response);
-    }
-  }
-  xhr.onerror = function() {
-    updateInfoWithError();
-    console.log('there was an error');
-  };
- }
+// ===============================
+// 1. RÉCUPÉRER LES ÉLÉMENTS DU DOM
+// ===============================
 
- //Display info on screen
- function updateInfo(resp){
-   
-  // ajax
-  let url = new URL(resp.homeworld);
-  url.protocol = 'https:'
-  xhr.open('GET', url.href);
+const button = document.querySelector("#button");
+const nameElement = document.querySelector("#name");
+const heightElement = document.querySelector("#height");
+const genderElement = document.querySelector("#gender");
+const birthYearElement = document.querySelector("#birth-year");
+const homeWorldElement = document.querySelector("#home-world");
 
-  xhr.responseType = 'json';
-  xhr.send();
-  xhr.onload = function(){
-    if(xhr.status != 200){
-      console.log('there was an error 2');
-    }
-    else{
-      updateInfo2(xhr.response)
-    }
-  }
-  xhr.onerror = function() {
-    console.log('there was an error 3');
-  };
- 	console.log(resp.homeworld);
-  console.log(resp.name);
- 	console.log(names);
 
-  names.innerText = resp.name;
-  height.innerText = `Height: ${resp.height}`;
-  gender.innerText = `Gender: ${resp.gender}`;
-  birthYear.innerText = `Birth Year: ${resp.birth_year}`;
-  // homeWorld.innerText = `Home World: ${planet}`;
+// ======================================
+// 2. RÉCUPÉRER UN PERSONNAGE ALÉATOIRE
+// ======================================
+
+function getInfo() {
+
+    // Afficher le chargement
+    updateWithLoading();
+
+    // Nombre aléatoire entre 1 et 83
+    const randomNumber = Math.floor(Math.random() * 83) + 1;
+
+    const apiUrl =
+        `https://www.swapi.tech/api/people/${randomNumber}`;
+
+    // Créer une nouvelle requête AJAX
+    const xhr = new XMLHttpRequest();
+
+    // Préparer la requête
+    xhr.open("GET", apiUrl);
+
+    // Demander une réponse JSON
+    xhr.responseType = "json";
+
+    // Envoyer la requête
+    xhr.send();
+
+
+    // Quand la réponse est reçue
+    xhr.onload = function () {
+
+        if (xhr.status !== 200) {
+
+            updateInfoWithError();
+            console.log(`Error: ${xhr.status}`);
+
+            return;
+        }
+
+        // Avec swapi.tech, les informations du personnage
+        // se trouvent dans result.properties
+        const character = xhr.response.result.properties;
+
+        // Récupérer le monde d'origine
+        getHomeWorld(character);
+    };
+
+
+    // En cas d'erreur réseau
+    xhr.onerror = function () {
+
+        updateInfoWithError();
+        console.log("Network error");
+
+    };
 }
 
 
-//Display Home World
-function updateInfo2(re){
- 	// console.log(re.name)
- 	// return re.name;
- 	homeWorld.innerText = `Home World: ${re.name}`;
+// ======================================
+// 3. RÉCUPÉRER LE MONDE D'ORIGINE
+// ======================================
+
+function getHomeWorld(character) {
+
+    const xhrPlanet = new XMLHttpRequest();
+
+    // character.homeworld contient l'URL de la planète
+    xhrPlanet.open("GET", character.homeworld);
+
+    xhrPlanet.responseType = "json";
+
+    xhrPlanet.send();
+
+
+    xhrPlanet.onload = function () {
+
+        if (xhrPlanet.status !== 200) {
+
+            updateInfoWithError();
+            console.log(`Planet error: ${xhrPlanet.status}`);
+
+            return;
+        }
+
+        // Les informations de la planète sont aussi
+        // dans result.properties
+        const planet = xhrPlanet.response.result.properties;
+
+        // Afficher le personnage et la planète
+        updateInfo(character, planet.name);
+    };
+
+
+    xhrPlanet.onerror = function () {
+
+        updateInfoWithError();
+        console.log("Planet network error");
+
+    };
 }
 
-//Display when Error
-function updateInfoWithError(){
-  names.innerText = 'Oh No! That person isnt available.';
-  height.innerText = ''
-  gender.innerText = ''
-  birthYear.innerText = ''
-  homeWorld.innerText = ''
+
+// ======================================
+// 4. AFFICHER LES INFORMATIONS
+// ======================================
+
+function updateInfo(character, planetName) {
+
+    nameElement.innerText = character.name;
+
+    heightElement.innerText =
+        `Height: ${character.height}`;
+
+    genderElement.innerText =
+        `Gender: ${character.gender}`;
+
+    birthYearElement.innerText =
+        `Birth Year: ${character.birth_year}`;
+
+    homeWorldElement.innerText =
+        `Home World: ${planetName}`;
 }
 
-//Display when updating info (pending data)
-function updateWithLoading(){
-  //Icon link: https://fontawesome.com/how-to-use/on-the-web/styling/animating-icons
-  names.innerHTML = '<i class="fa fa-spinner fa-pulse fa-3x fa-fw"></i> <p>Loading...</p>';
-  height.innerText = ''
-  gender.innerText = ''
-  birthYear.innerText = ''
-  homeWorld.innerText = ''
+
+// ======================================
+// 5. AFFICHER UNE ERREUR
+// ======================================
+
+function updateInfoWithError() {
+
+    nameElement.innerText =
+        "Oh No! That person isn't available.";
+
+    heightElement.innerText = "";
+    genderElement.innerText = "";
+    birthYearElement.innerText = "";
+    homeWorldElement.innerText = "";
 }
 
-button.addEventListener('click', getInfo);
+
+// ======================================
+// 6. AFFICHER LE CHARGEMENT
+// ======================================
+
+function updateWithLoading() {
+
+    nameElement.innerHTML = `
+        <i class="fa-solid fa-spinner fa-spin"></i>
+        <p>Loading...</p>
+    `;
+
+    heightElement.innerText = "";
+    genderElement.innerText = "";
+    birthYearElement.innerText = "";
+    homeWorldElement.innerText = "";
+}
 
 
+// ======================================
+// 7. ÉVÉNEMENT DU BOUTON
+// ======================================
 
-//https://mathiasbynens.be/notes/xhr-responsetype-json
-// Ref: https://www.w3schools.com/js/js_json_http.asp
-// Ref: https://www.freecodecamp.org/forum/t/get-json-with-the-javascript-xmlhttprequest-method/197547
+button.addEventListener("click", getInfo);
