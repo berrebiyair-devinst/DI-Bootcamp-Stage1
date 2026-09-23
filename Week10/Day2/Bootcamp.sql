37b870c1de5709495562bf07a550f703fa52dbd5
Yaïr <berrebiyair59@gmail.com>
2026-09-23 12:56:57 +0200

    Replace database dumps with SQL solutions

--- Week10/Day2/Bootcamp.sql
+++ Week10/Day2/Bootcamp.sql
@@ -1,103 +1,86 @@
---
--- PostgreSQL database dump
---
 
-\restrict rEeb03HwKuuVbgO5TvxFO9ztWII2EIuOXbifm9YMrqOQlwIhQWGbnZioE251M7K
 
--- Dumped from database version 18.6
--- Dumped by pg_dump version 18.6
+DROP TABLE IF EXISTS students;
 
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
-SET transaction_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SELECT pg_catalog.set_config('search_path', '', false);
-SET check_function_bodies = false;
-SET xmloption = content;
-SET client_min_messages = warning;
-SET row_security = off;
-
-SET default_tablespace = '';
-
-SET default_table_access_method = heap;
-
---
--- Name: students; Type: TABLE; Schema: public; Owner: postgres
---
-
-CREATE TABLE public.students (
-    student_id integer NOT NULL,
-    first_name character varying(50) NOT NULL,
-    last_name character varying(50) NOT NULL,
-    birth_date date NOT NULL
+CREATE TABLE students (
+    student_id SERIAL PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    birth_date DATE NOT NULL
 );
+INSERT INTO students (first_name, last_name, birth_date)
+VALUES
+    ('Marc', 'Benichou', '1998-11-02'),
+    ('Yoan', 'Cohen', '2010-12-03'),
+    ('Lea', 'Benichou', '1987-07-27'),
+    ('Amelie', 'Dux', '1996-04-07'),
+    ('David', 'Grez', '2003-06-14'),
+    ('Omer', 'Simpson', '1980-10-03');
+SELECT * FROM students;
+
+SELECT first_name, last_name
+FROM students;
 
 
-ALTER TABLE public.students OWNER TO postgres;
-
---
--- Name: students_student_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
---
-
-CREATE SEQUENCE public.students_student_id_seq
-    AS integer
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
 
 
-ALTER SEQUENCE public.students_student_id_seq OWNER TO postgres;
-
---
--- Name: students_student_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
---
-
-ALTER SEQUENCE public.students_student_id_seq OWNED BY public.students.student_id;
+SELECT first_name, last_name
+FROM students
+WHERE student_id = 2;
 
 
---
--- Name: students student_id; Type: DEFAULT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.students ALTER COLUMN student_id SET DEFAULT nextval('public.students_student_id_seq'::regclass);
 
 
---
--- Data for Name: students; Type: TABLE DATA; Schema: public; Owner: postgres
---
-
-COPY public.students (student_id, first_name, last_name, birth_date) FROM stdin;
-1	Marc	Benichou	1998-11-02
-2	Yoan	Cohen	2010-12-03
-3	Lea	Benichou	1987-07-27
-4	Amelie	Dux	1996-04-07
-5	David	Grez	2003-06-14
-6	Omer	Simpson	1980-10-03
-\.
+SELECT first_name, last_name
+FROM students
+WHERE last_name = 'Benichou'
+AND first_name = 'Marc';
 
 
---
--- Name: students_student_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
---
 
-SELECT pg_catalog.setval('public.students_student_id_seq', 6, true);
-
-
---
--- Name: students students_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.students
-    ADD CONSTRAINT students_pkey PRIMARY KEY (student_id);
+SELECT first_name, last_name
+FROM students
+WHERE last_name = 'Benichou'
+OR first_name = 'Marc';
 
 
---
--- PostgreSQL database dump complete
---
 
-\unrestrict rEeb03HwKuuVbgO5TvxFO9ztWII2EIuOXbifm9YMrqOQlwIhQWGbnZioE251M7K
+
+SELECT first_name, last_name
+FROM students
+WHERE first_name ILIKE '%a%';
+
+
+
+
+SELECT first_name, last_name
+FROM students
+WHERE first_name ILIKE 'a%';
+
+
+
+
+SELECT first_name, last_name
+FROM students
+WHERE first_name ILIKE '%a';
+
+
+
+
+SELECT first_name, last_name
+FROM students
+WHERE first_name ILIKE '%a_';
+
+
+
+
+SELECT first_name, last_name
+FROM students
+WHERE student_id IN (1, 3);
+
+
+
+SELECT *
+FROM students
+WHERE birth_date >= '2000-01-01';
 
--- Week10/Day2/Public.sql
+++ Week10/Day2/Public.sql
@@ -1,169 +1,50 @@
---
--- PostgreSQL database dump
---
+CREATE DATABASE "Public";
 
-\restrict v9bjWXZujgJTZ00qwaZaEjt0yvV1Kq3WMVI5B7dI2OcpqJUFdrhHEKysOGKZLto
-
--- Dumped from database version 18.6
--- Dumped by pg_dump version 18.6
-
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
-SET transaction_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SELECT pg_catalog.set_config('search_path', '', false);
-SET check_function_bodies = false;
-SET xmloption = content;
-SET client_min_messages = warning;
-SET row_security = off;
-
-SET default_tablespace = '';
-
-SET default_table_access_method = heap;
-
---
--- Name: customers; Type: TABLE; Schema: public; Owner: postgres
---
-
-CREATE TABLE public.customers (
-    customer_id integer NOT NULL,
-    first_name character varying(50) NOT NULL,
-    last_name character varying(50) NOT NULL
+CREATE TABLE items (
+    item_id SERIAL PRIMARY KEY,
+    item_name VARCHAR(100) NOT NULL,
+    price INTEGER NOT NULL
 );
 
-
-ALTER TABLE public.customers OWNER TO postgres;
-
---
--- Name: customers_customer_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
---
-
-CREATE SEQUENCE public.customers_customer_id_seq
-    AS integer
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
-ALTER SEQUENCE public.customers_customer_id_seq OWNER TO postgres;
-
---
--- Name: customers_customer_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
---
-
-ALTER SEQUENCE public.customers_customer_id_seq OWNED BY public.customers.customer_id;
-
-
---
--- Name: items; Type: TABLE; Schema: public; Owner: postgres
---
-
-CREATE TABLE public.items (
-    item_id integer NOT NULL,
-    item_name character varying(100) NOT NULL,
-    price integer NOT NULL
+CREATE TABLE customers (
+    customer_id SERIAL PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL
 );
 
+INSERT INTO items (item_name, price)
+VALUES
+    ('Small Desk', 100),
+    ('Large Desk', 300),
+    ('Fan', 80);
 
-ALTER TABLE public.items OWNER TO postgres;
+INSERT INTO customers (first_name, last_name)
+VALUES
+    ('Greg', 'Jones'),
+    ('Sandra', 'Jones'),
+    ('Scott', 'Scott'),
+    ('Trevor', 'Green'),
+    ('Melanie', 'Johnson');
 
---
--- Name: items_item_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
---
+SELECT *
+FROM items;
 
-CREATE SEQUENCE public.items_item_id_seq
-    AS integer
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
+SELECT *
+FROM items
+WHERE price > 80;
 
+SELECT *
+FROM items
+WHERE price <= 300;
 
-ALTER SEQUENCE public.items_item_id_seq OWNER TO postgres;
+SELECT *
+FROM customers
+WHERE last_name = 'Smith';
 
---
--- Name: items_item_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
---
+SELECT *
+FROM customers
+WHERE last_name = 'Jones';
 
-ALTER SEQUENCE public.items_item_id_seq OWNED BY public.items.item_id;
-
-
---
--- Name: customers customer_id; Type: DEFAULT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.customers ALTER COLUMN customer_id SET DEFAULT nextval('public.customers_customer_id_seq'::regclass);
-
-
---
--- Name: items item_id; Type: DEFAULT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.items ALTER COLUMN item_id SET DEFAULT nextval('public.items_item_id_seq'::regclass);
-
-
---
--- Data for Name: customers; Type: TABLE DATA; Schema: public; Owner: postgres
---
-
-COPY public.customers (customer_id, first_name, last_name) FROM stdin;
-1	Greg	Jones
-2	Sandra	Jones
-3	Scott	Scott
-4	Trevor	Green
-5	Melanie	Johnson
-\.
-
-
---
--- Data for Name: items; Type: TABLE DATA; Schema: public; Owner: postgres
---
-
-COPY public.items (item_id, item_name, price) FROM stdin;
-1	Small Desk	100
-2	Large Desk	300
-3	Fan	80
-\.
-
-
---
--- Name: customers_customer_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
---
-
-SELECT pg_catalog.setval('public.customers_customer_id_seq', 5, true);
-
-
---
--- Name: items_item_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
---
-
-SELECT pg_catalog.setval('public.items_item_id_seq', 3, true);
-
-
---
--- Name: customers customers_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.customers
-    ADD CONSTRAINT customers_pkey PRIMARY KEY (customer_id);
-
-
---
--- Name: items items_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.items
-    ADD CONSTRAINT items_pkey PRIMARY KEY (item_id);
-
-
---
--- PostgreSQL database dump complete
---
-
-\unrestrict v9bjWXZujgJTZ00qwaZaEjt0yvV1Kq3WMVI5B7dI2OcpqJUFdrhHEKysOGKZLto
-
+SELECT *
+FROM customers
+WHERE first_name != 'Scott';
